1b7b4a8ea89e482313a08ba458c0086098bf3995
AceBurgundy <Samadriansabalo99@gmail.com>
2022-08-28 19:24:40 +0800

    Added a new if else to remove() and added comments.

--- queueEngine.js
+++ queueEngine.js
@@ -54,28 +54,44 @@ export class Queue {
     }
 
     remove() {
-        // this will check if there are no items to be removed
-        let allEmptyStrings = this.array.every(element => element == "")
-        console.log(allEmptyStrings);
-        if (this.array.length <= 0 || allEmptyStrings) {
+        let countAllEmptyStrings = 0
+            // this will count all empty strings
+        for (let index = 0; index < this.array.length; index++) {
+            if (this.array[index] == "") {
+                countAllEmptyStrings++
+            }
+        }
+        // if there is only one letter to remove reset the queue
+        if (countAllEmptyStrings == this.array.length - 1) {
+            this.valuesAdded -= 1
+            this.array[this.first] = ""
+            this.array = []
+            this.first = 0
+            this.last = 0
+                // if the array is empty or the array contains only empty strings 
+        } else if (this.array.length <= 0 || countAllEmptyStrings == this.array.length) {
             return "Queue underflow"
+                // if the array only has 1 value and the variable holding the first is not empty
         } else if (this.array.length == 1 && this.array[this.first] != "") {
             this.array[this.first] = ""
             this.valuesAdded -= 1
             this.first = 0;
+            // if the length of the array is equals to its size and the variable holding the first is not empty and the next value to the first is an empty string
         } else if (this.array.length == this.size && this.array[this.first] != "" && this.array[this.first + 1] == "") {
             this.array[this.first] = ""
             this.valuesAdded -= 1
             this.first = 0;
+            // if the length of the array is equals to its size and the the variable holding the last is not empty and the prev value to the last is an empty string
         } else if (this.array.length == this.size && this.array[this.last] != "" && this.array[this.last - 1] == "") {
             this.array[this.first] = ""
             this.valuesAdded -= 1
             this.first = 0;
             this.last = 0;
+            // if there are no values added
         } else if (this.valuesAdded == 0) {
             return
         } else {
-            /* If there is, the element on the index pointed by first will be removed */
+            // If there is, the element on the index pointed by first will be removed
             this.array[this.first] = ""
             this.valuesAdded -= 1
             this.first == this.size - 1 ? this.first = 0 : this.first += 1
