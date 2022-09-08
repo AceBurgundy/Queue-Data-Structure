17faa42b51b5782cd1d6a1f49fbc8a7760d7b1e9
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-08 16:23:36 +0800

    Allowed inputs for 2 values.

--- queueEngine.js
+++ queueEngine.js
@@ -45,7 +45,7 @@ export class Queue {
             } else {
                 /* this will allow values to be added to the array if
                     the length of the array is not equal to the size of the array */
-                if (element.length > 1) {
+                if (element.length > 2) {
                     return
                 } else if (this.array.length != this.size) {
                     this.array.push(element)
