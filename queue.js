a9b3facb5f62b0dbc1405b3b5ce40a226eaa1dc2
Sam Adrian <samadriansabalo99@gmail.com>
2022-10-22 17:35:20 +0800

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,18 +1,26 @@
 export class Queue {
 
-    array = []
-    size
-    first = 0
-    queuehasNoSize = false
-    last = 0
-    valuesAdded = 0
+    constructor() {
+        this.queue = []
+        this.size //default queue size
+        this.first = 0
+        this.hasNoSize = false
+        this.last = 0
+        this.valuesAdded = 0 //shows all values added (not including "")
+    }
 
-    empty() {
-        if (this.array.length == 0) {
+    isEmpty() {
+
+        if (this.valuesAdded == 0) {
             return true
-        } else {
-            return false
         }
+
+        return false
+
+    }
+
+    getQueue() {
+        return this.queue
     }
 
     getFirst() {
@@ -23,84 +31,83 @@ export class Queue {
         return this.last
     }
 
-    getQueue() {
-        return this.array
-    }
-
-    insert(element) {
-        if (element.length <= 2 && element.length > 0) {
-            if (this.queuehasNoSize) {
-                this.size += 1
-                this.array.push(element)
-                this.last = this.array.lastIndexOf(element)
-                this.valuesAdded += 1
-            } else {
-                if (this.valuesAdded == this.size) {
-                    return "Queue overflow!";
-                } else if (this.array.length != this.size) {
-                    // adds another element if size is still not equal to the length
-                    this.array.push(element)
-                    this.last = this.array.lastIndexOf(element)
-                    this.valuesAdded += 1
-                } else {
-                    // finds the index of the first "" and swaps it to the user's input
-                    let firstEmptyString = this.array.indexOf("")
-                    this.array[firstEmptyString] = element
-                    this.valuesAdded += 1
-                    this.last = firstEmptyString
-                }
-            }
-        } else {
-            return
-        }
-    }
-
     getSize() {
         return this.size
     }
 
-    remove() {
-        if (this.queuehasNoSize) {
-            if (this.array.length <= 0) {
-                return "Queue underflow"
-            } else if (this.array.length == 1 && this.array[0] == "") {
-                return "Queue underflow"
-            } else if (this.array.length == 1) {
-                this.array.splice(this.first, 1)
-                this.valuesAdded -= 1
-                this.size = 0
-            } else {
-                this.size -= 1
-                this.array.splice(this.first, 1)
-                this.valuesAdded -= 1
-                this.last = this.array.length - 1
-            }
-        } else {
-            if (this.valuesAdded == 0) {
-                return "Queue underflow"
-            } else if (this.first == this.last) {
-                this.array = []
-                this.valuesAdded = 0
-                this.first = 0;
-                this.last = 0;
-            } else {
-                // change element to be removed to an empty string
-                this.array[this.first] = ""
-                this.valuesAdded -= 1
-                this.first == this.size - 1 ? this.first = 0 : this.first += 1
-            }
+    insert(element) {
+
+        if (this.hasNoSize) {
+
+            this.size += 1
+            this.queue.push(element)
+            this.last = this.queue.lastIndexOf(element)
+            this.valuesAdded += 1
+            return
         }
+
+        if (this.queue.length != this.size) {
+            this.queue.push(element)
+            this.last = this.queue.lastIndexOf(element)
+            this.valuesAdded += 1
+
+        } else {
+
+            if (this.queue[this.last + 1] == "") {
+                this.queue[this.last + 1] = element
+                this.valuesAdded += 1
+                this.last = this.last + 1
+                return
+            }
+
+            let firstEmptyString = this.queue.indexOf("")
+            this.queue[firstEmptyString] = element
+            this.valuesAdded += 1
+            this.last = firstEmptyString
+        }
+    }
+
+    overflow() {
+        if (!this.hasNoSize) {
+            return this.valuesAdded == this.size;
+        }
+    }
+
+    remove() {
+
+        if (this.hasNoSize) {
+
+            this.size -= 1
+            this.queue.shift()
+            this.last == 0 ? this.last == 0 : this.last = this.queue.length - 1
+            this.valuesAdded -= 1
+            return
+        }
+
+        if (this.first == this.last) {
+            this.queue = []
+            this.valuesAdded = 0
+            this.first = 0;
+            this.last = 0;
+            return
+        }
+
+        this.queue[this.first] = ""
+        this.valuesAdded -= 1
+        this.first == this.size - 1 ? this.first = 0 : this.first += 1
+
     }
 
     playWithNoSize(approved) {
         if (approved) {
-            this.queuehasNoSize = true
+            this.hasNoSize = true
         } else {
-            this.queuehasNoSize = false
+            this.hasNoSize = false
         }
     }
 
     setSize(size) {
         this.size = size
     }
+
 }
